a5819c285d1288fe3c9fdba360bf45f14e49a150
YeopJ <cjinyeop@gmail.com>
2025-06-14 14:46:53 +0900

    메인페이지 작성

--- src/main/java/com/baeksoo/shop/BasicController.java
+++ src/main/java/com/baeksoo/shop/BasicController.java
@@ -1,19 +1,31 @@
 package com.baeksoo.shop;
 
+import com.baeksoo.shop.item.Item;
+import com.baeksoo.shop.item.ItemRepository;
+import com.baeksoo.shop.item.ItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+
+
+import java.util.List;
 
 
 @Controller
+@RequiredArgsConstructor
 public class BasicController {
+    private final ItemRepository itemRepository;
+    private final ItemService itemService;
+
     @GetMapping("/")
-    String hello(){
-        return "../static/index.html";
+    String getMainPage(Model model){
+        List<Item> result = itemService.findAllItem();
+
+        model.addAttribute("items", result);
+
+        return "index.html";
     }
-
-
 }
